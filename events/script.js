4c6f74fcac72505456492ff2d359a8c6e54e5b2e
Justin Michel <justinmichel2000@gmail.com>
2023-09-22 16:26:21 +0200

    keypress

--- events/script.js
+++ events/script.js
@@ -3,6 +3,8 @@ let longname = document.querySelector(".displayedsquare-wrapper");
 
 let actionlog = document.querySelector("h2");
 
+let body = document.querySelector("body");
+
 
 const _initTime = Date.now()
 
@@ -38,6 +40,37 @@ for (let actionSquare of actionSquares) {
 }
 
 
+
+
+
+
+function k (keypress){
+
+    console.log(keypress);
+
+    if (keypress.keyCode == 32) {
+
+        color();
+       
+    }
+}
+
+function color() {
+    
+    let randomBetween = (min, max) =>
+    min + Math.floor(Math.random() * (max - min + 1));
+    
+    let r = randomBetween(0, 255);
+    let g = randomBetween(0, 255);
+    let b = randomBetween(0, 255);
+    
+    let rgb = `rgb(${r},${g},${b})`;
+    
+    body.style.backgroundColor = rgb;
+    
+     }
+
+body.addEventListener("keypress" , k);
 
 
 
