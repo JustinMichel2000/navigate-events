bdb442da0d45eb00c80d25c31dbd33dc36332e40
Justin Michel <justinmichel2000@gmail.com>
2023-09-22 15:15:18 +0200

    1

--- events/script.js
+++ events/script.js
@@ -1,3 +1,9 @@
+
+let longname = document.querySelector(".displayedsquare-wrapper");
+
+let actionlog = document.querySelector("h2");
+
+
 const _initTime = Date.now()
 
 const getElapsedTime = () => {
@@ -5,11 +11,35 @@ const getElapsedTime = () => {
 }
 
 const clickOnSquare = (e) => {
+
+  let div = document.createElement("div");
+  div.classList.add("displayedsquare" , e.target.classList[1]);
+
+  longname.appendChild(div);
+
   console.log(e.target.classList[1])
   console.log(getElapsedTime())
+
+
+ let li = document.createElement("li");
+
+ actionlog.appendChild(li);
+
+ let actiontext = document.createTextNode( "[" + getElapsedTime() + "]" + " Created a new " + e.target.classList[1] + " square");
+
+ li.appendChild(actiontext);
+
 }
 
 const actionSquares = document.querySelectorAll('.actionsquare')
 for (let actionSquare of actionSquares) {
   actionSquare.addEventListener('click', clickOnSquare)
+  
 }
+
+
+
+
+
+
+
